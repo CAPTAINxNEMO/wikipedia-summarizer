ed619addbeafe5cea7795e495c68c8257ce150b7
CAPTAINxNEMO <shashank.v.bhave@gmail.com>
2024-03-12 23:37:37 +0530

    base for online pdf file parsing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+# import PyPDF2 as pdf
 from bs4 import BeautifulSoup as bs
 
 file = open("webpage_content.txt", "r+", encoding = "utf-8")
@@ -6,12 +7,14 @@ file.truncate(0)
 
 url = input("Wep page link: ")
 response = requests.get(url)
-content = response.text
 
-soup = bs(content, "html.parser")
-for script in soup(["script", "style"]):
-    script.extract()
-for text in soup.find_all(string = True):
-    file.write(text)
+if url.endswith(".pdf"):
+    print("Placeholder")
+else:
+    soup = bs(response.content, "html.parser")
+    for script in soup(["script", "style"]):
+        script.extract()
+    for text in soup.find_all(string = True):
+        file.write(text)
 
 file.close()
