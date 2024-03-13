6ca8831c5ddc02ac8c122b4e1573c7e30cf397ff
CAPTAINxNEMO <shashank.v.bhave@gmail.com>
2024-03-13 12:17:57 +0530

    text printing

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import requests
+import wikipediaapi
 from bs4 import BeautifulSoup as bs
 # import re
 # import nltk
 
-def webParser():
-    # url = input("Wep page link: ")
-    url = "https://en.wikipedia.org/wiki/Severe_acute_respiratory_syndrome_coronavirus_2"
-    response = requests.get(url)
+# url = input("Wep page link: ")
+url = "https://en.wikipedia.org/wiki/Severe_acute_respiratory_syndrome_coronavirus_2"
+response = requests.get(url)
 
-    soup = bs(response.content, "html.parser")
-    for script in soup(["script", "style"]):
-        script.extract()
-    for text in soup.find_all(string = True):
-        print(text)
+soup = bs(response.content, "html.parser")
+content_div = soup.find("div", id = "bodyContent")
+paragraphs = content_div.find_all("p")
 
-def summarizer():
-    print("Placeholder")
-
-if __name__ == "__main__":
-    webParser()
-    summarizer()
+text = ""
+for p in paragraphs:
+    text += p.get_text() + "\n"
+print(text)
